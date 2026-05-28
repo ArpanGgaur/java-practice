327ae935b8c90d23690815e842375b7b8b56e14e
ArpanGgaur <arpanggaur@gmail.com>
2026-05-28 16:18:50 +0530

    Add files via upload

---
+++ HelloWord.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Day 1 Complete");
+        System.out.println("Arpan Gaur - Selenium Automation Tester");
+    }
+}
